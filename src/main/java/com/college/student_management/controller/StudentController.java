f3b920086c5aae1af13c0b6ec3a19b24affe10e6
Rithish <rithishs094@gmail.com>
2026-09-17 15:50:51 +0530

    second commit

--- src/main/java/com/college/student_management/controller/StudentController.java
+++ src/main/java/com/college/student_management/controller/StudentController.java
@@ -3,8 +3,17 @@ package com.college.student_management.controller;
 import com.college.student_management.entity.Student;
 import com.college.student_management.service.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 public class StudentController {
@@ -27,6 +36,11 @@ public class StudentController {
     //     return "Successful";
     // }
 
+        // @GetMapping("/addtwoint")
+        // public int addTwoInt() {
+        //     return 1+2;
+        // }
+
     // @PostMapping("/students")
     // public Student createStudent(@RequestBody Student student) {
     //     return studentService.createStudent(student);
@@ -45,4 +59,32 @@ public class StudentController {
     public Student createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
     }
+
+    //GET ALL STUDENTS
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    //GET STUDENT BY ID
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+
+    //UPDATE STUDENT
+    @PutMapping("/students/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    //DELETE STUDENT
+    @DeleteMapping ("/students/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+    
+
+
 }
--- src/main/java/com/college/student_management/service/StudentService.java
+++ src/main/java/com/college/student_management/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.college.student_management.service;
 
+import java.util.List;
+
 import com.college.student_management.entity.Student;
 import com.college.student_management.repository.StudentRepository;
 import org.springframework.stereotype.Service;
@@ -19,13 +21,44 @@ public class StudentService {
 
     //DAY 2
 
+
     private final StudentRepository studentRepository;
+
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
+
+
     public Student createStudent(Student student) {
         return studentRepository.save(student);
     }
+
+
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+
+     public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public Student updateStudent(Long id, Student updatedStudent) {
+        Student existingStudent = studentRepository.findById(id).orElse(null);
+        if (existingStudent != null) {
+            existingStudent.setName(updatedStudent.getName());
+            existingStudent.setEmail(updatedStudent.getEmail());
+            existingStudent.setDepartment(updatedStudent.getDepartment());
+            return studentRepository.save(existingStudent);
+        }
+        return null;
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
 }
